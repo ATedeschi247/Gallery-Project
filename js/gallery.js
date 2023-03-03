15f97bcdbe8cf72373db630aded1dd7a5fde8b79
ATedeschi247 <84191428+ATedeschi247@users.noreply.github.com>
2023-03-03 09:30:09 -0500

    Add files via upload
    Kept working on the project and made some good progress too :)

--- js/gallery.js
+++ js/gallery.js
@@ -18,27 +18,34 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
+  requestAnimFrame( animate );
 	var currentTime = new Date().getTime();
 	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
+    mLastFrameTime = currentTime;
 	}
-
+  
 	if ((currentTime - mLastFrameTime) > mWaitTime) {
-		swapPhoto();
+    swapPhoto();
 		mLastFrameTime = currentTime;
 	}
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+let photo;
 
 function swapPhoto() {
-	document.getElementById('photo').setAttribute('src', '')
-	console.log('swap photo');
+  photo = mImages.images[mCurrentIndex];
+	document.getElementById('photo').setAttribute('src', photo.imgPath);
+  GalleryImage();
+  if(mCurrentIndex >= 12) {
+    mCurrentIndex = 0;
+  } else {
+    mCurrentIndex++;
+  }
 }
 
 // Counter for the mImages array
-var mCurrentIndex = 0;
+var mCurrentIndex = 1;
 
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
@@ -57,45 +64,49 @@ var mUrl = 'images.json';
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
+  return function(e) {
+    galleryImage.img = e.target;
 		mImages.push(galleryImage);
 	}
 }
 
 $(document).ready( function() {
-	
-	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
+  
+  // This initially hides the photos' metadata information
+	// $('.details').eq(0).hide();
 	
 });
 
 window.addEventListener('load', function() {
-	
+  
 	console.log('window loaded');
-
+  
 }, false);
-function fetchJSON(callback) {
-  mRequest.addEventListener('readystatechange', function() {
-    if(mRequest.readyState == 4 && mRequest.status == 200) {
+fetchJSON();
+function fetchJSON() {
+  mRequest.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
       mImages = JSON.parse(mRequest.responseText);
-      callback(mImages);
-    } else if(mRequest.readyState == 4){
-      callback('Could not get data');
     }
-  });
+  };
   mRequest.open('GET', mUrl);
-  mRequest.setRequestHeader()
   mRequest.send();
 }
 
 function GalleryImage() {
-	//implement me as an object to hold the following data about an image:
-	var location;
-	var description;
-	var date;
-	var img;
+  //implement me as an object to hold the following data about an image:
+	var location = photo.imgLocation;
+	var description = photo.description;
+	var date = photo.date;
+
+  document.querySelector('.location').innerHTML = "Location: " + location;
+  document.querySelector('.description').innerHTML = "Description: " + description;
+  document.querySelector('.date').innerHTML = "Date: " + date.toString();
 }
-fetchJSON(function(response) {
-  console.log(response);
+document.querySelector('.moreIndicator').addEventListener('click', function() {
+  if(this.classList.contains('rot90')) {
+    this.classList.replace('rot90', 'rot270');
+  } else {
+    this.classList.replace('rot270', 'rot90');
+  }
 });
