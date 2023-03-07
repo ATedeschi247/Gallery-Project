a544542d8c985c39ed371cb403124dd948cc7f28
ATedeschi247 <84191428+ATedeschi247@users.noreply.github.com>
2023-03-06 21:26:40 -0500

    Add files via upload
    Finished this part of the site at home, where I fixed the bugs in the JavaScript and got the conditional JSON url to work correctly.

--- js/gallery.js
+++ js/gallery.js
@@ -33,13 +33,13 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 let photo;
 
-function swapPhoto() {
-  photo = mImages.images[mCurrentIndex];
-	document.getElementById('photo').setAttribute('src', photo.imgPath);
-  GalleryImage();
-  if(mCurrentIndex >= 4) {
-    mCurrentIndex = 0;
+function swapPhoto() {                                                // This code goes through the JSON-made array of images. Due to how it
+  if(mCurrentIndex >= mImages.images.length) {                        // repeats every 5 seconds, I don't need a for loop and can just alter the
+    mCurrentIndex = 0;                                                // mCurrent Index within the function.
   } else {
+    photo = mImages.images[mCurrentIndex];
+    document.getElementById('photo').setAttribute('src', photo.imgPath);
+    GalleryImage();
     mCurrentIndex++;
   }
 }
@@ -58,7 +58,7 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'extra.json';
+var mUrl = 'images.json';
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
@@ -69,11 +69,19 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 	}
 }
 
-$(document).ready( function() {
-  
-  // This initially hides the photos' metadata information
-	// $('.details').eq(0).hide();
-	
+$(document).ready( function() {                                             // This code searches for the 'extra.json' file via another XMLHttpRequest.
+  mJson = new XMLHttpRequest();                                             // If it exists, sets mUrl to that extra.json file and runs fetchJSON.
+  mJson.open('GET', 'efxtra.json');                                          // Otherwise, it leaves mUrl as 'images.json' and runs fetchJSON as that.
+  mJson.onreadystatechange = function() {
+    if(mJson.readyState == 4 && mJson.status == 200) {
+      mUrl = 'extra.json';
+      fetchJSON();
+    }
+    if(mJson.readyState == 4 && mJson.status != 200) {
+      fetchJSON();
+    }
+  };
+	mJson.send();
 });
 
 window.addEventListener('load', function() {
@@ -81,9 +89,8 @@ window.addEventListener('load', function() {
 	console.log('window loaded');
   
 }, false);
-fetchJSON();
-function fetchJSON() {
-  mRequest.onreadystatechange = function() {
+function fetchJSON() {                                                      // This gets the JSON file in question, iterates thorugh it, and
+  mRequest.onreadystatechange = function() {                                // assigns each item to an item in the array mImages.
     if(this.readyState == 4 && this.status == 200) {
       mImages = JSON.parse(mRequest.responseText);
     }
@@ -92,8 +99,8 @@ function fetchJSON() {
   mRequest.send();
 }
 
-function GalleryImage() {
-  //implement me as an object to hold the following data about an image:
+function GalleryImage() {                                                   // I used this function as a shorthand way to update the index.html
+  //implement me as an object to hold the following data about an image:    // whenever I changed the mCurrentIndex.
 	var location = photo.imgLocation;
 	var description = photo.description;
 	var date = photo.date;
@@ -102,8 +109,8 @@ function GalleryImage() {
   document.querySelector('.description').innerHTML = "Description: " + description;
   document.querySelector('.date').innerHTML = "Date: " + date.toString();
 }
-document.querySelector('.moreIndicator').addEventListener('click', function() {
-  if(this.classList.contains('rot90')) {
+document.querySelector('.moreIndicator').addEventListener('click', function() {           // Changes rotation of .moreIndicator when clicked
+  if(this.classList.contains('rot90')) {                                                  // via class change. Also implements the fade effect.
     this.classList.replace('rot90', 'rot270');
     $( ".details" ).first().fadeToggle( "slow", "linear" );
 
@@ -113,12 +120,17 @@ document.querySelector('.moreIndicator').addEventListener('click', function() {
   }
 });
 
-document.querySelector('#nextPhoto').addEventListener('click', function() {
-  swapPhoto();
+document.querySelector('#nextPhoto').addEventListener('click', function() {              // These functions add event listeners to each of the navigation
+  if(mCurrentIndex == mImages.images.length) {                                                               // buttons and assigns them each their behaviors, altering
+    mCurrentIndex = 0;                                                                   // the mCurrentIndex and running swapPhoto when needed.
+    swapPhoto();
+  } else {
+    swapPhoto();
+  }
 });
 document.querySelector('#prevPhoto').addEventListener('click', function() {
-  if(mCurrentIndex == 1) {
-    mCurrentIndex = 4;
+  if(mCurrentIndex <= 1) {
+    mCurrentIndex = mImages.images.length - 1;
     swapPhoto();
   } else {
     mCurrentIndex -= 2;
